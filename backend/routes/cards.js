ef689a3e0f67327be712c9079844f6a5067ca554
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-11-17 10:37:15 +0300

    api testinh

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -7,19 +7,19 @@ const { cardValidate } = require('../Validations/cards');
 
 module.exports = router;
 
-router.get('/cards', findCards);
-router.post('/cards', cardValidate, postCard);
-router.delete('/cards/:cardId', celebrate({
+router.get('/api/cards', findCards);
+router.post('/api/cards', cardValidate, postCard);
+router.delete('/api/cards/:cardId', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
 }), deleteCard);
-router.put('/cards/:cardId/likes', celebrate({
+router.put('/api/cards/:cardId/likes', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
 }), setLikeToCard);
-router.delete('/cards/:cardId/likes', celebrate({
+router.delete('/api/cards/:cardId/likes', celebrate({
   [Segments.PARAMS]: Joi.object().keys({
     cardId: Joi.string().length(24).hex().required(),
   }),
