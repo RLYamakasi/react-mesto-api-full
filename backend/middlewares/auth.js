961f426fa405cfb722c2a45e9f0dd3c4b7a19616
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-11-17 16:38:29 +0300

    api через header

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,10 +1,13 @@
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const { JWT_SECRET } = process.env;
 const ErrorLogin = require('../errors/errorlogin');
 
 module.exports.auth = (req, res, next) => {
-  const cookie = req.cookies.token;
+  const token = req.headers.authorization;
   try {
-    const tokenCheck = jwt.verify(cookie, 'some-secret-key');
+    const tokenCheck = jwt.verify(token, JWT_SECRET);
     if (!tokenCheck) {
       return next(new ErrorLogin('Что-то пошло не так'));
     }
@@ -12,5 +15,16 @@ module.exports.auth = (req, res, next) => {
     return next();
   } catch (err) {
     return next(new ErrorLogin('Что-то пошло не так'));
+  }
+};
+
+module.exports.signToken = (id) => {
+  try {
+    console.log(JWT_SECRET);
+    const token = jwt.sign({ _id: id }, JWT_SECRET);
+    return token;
+  } catch (err) {
+    console.log(err);
+    return false;
   }
 };
