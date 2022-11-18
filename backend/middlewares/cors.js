f5051076d7ad3acef29d257ba3c53af101a2448c
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-11-18 14:12:44 +0300

    api через куки пробуем+++++++++++

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,6 +12,7 @@ module.exports.corsCheck = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Methods', '*');
   }
 
   if (method === 'OPTIONS') {
