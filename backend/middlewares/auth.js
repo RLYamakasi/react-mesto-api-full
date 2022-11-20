badafd9522f9b163361fc47487482242b8e5d492
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-11-20 20:37:57 +0300

    на тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-const { NODE_ENV = 'development', JWT_SECRET = 'my-cO0lL-pazz' } = process.env;
 const jwt = require('jsonwebtoken');
 const ErrorLogin = require('../errors/errorlogin');
 
 module.exports.auth = (req, res, next) => {
   const cookie = req.cookies.token;
+  const { NODE_ENV, JWT_SECRET } = process.env;
   try {
-    const tokenCheck = jwt.verify(cookie, NODE_ENV === 'production' ? JWT_SECRET : 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b');
+    const tokenCheck = jwt.verify(cookie, NODE_ENV === 'devolepment' ? JWT_SECRET : 'supersecretkey');
     if (!tokenCheck) {
       return next(new ErrorLogin('Что-то пошло не так'));
     }
