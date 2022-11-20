716a6d559f961426d932cdd2ff0f180f2ebd99a1
Алексей Фёдоров <67751797+RLYamakasi@users.noreply.github.com>
2022-11-20 15:48:36 +0300

    на тест

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,6 +6,7 @@ module.exports.auth = (req, res, next) => {
   const cookie = req.cookies.token;
   try {
     const tokenCheck = jwt.verify(cookie, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'eb28135ebcfc17578f96d4d65b6c7871f2c803be4180c165061d5c2db621c51b');
+    console.log(tokenCheck);
     if (!tokenCheck) {
       return next(new ErrorLogin('Что-то пошло не так'));
     }
